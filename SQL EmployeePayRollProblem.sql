c8bcd9b615afc3fec8fa8db3d9459dcd178bdb30
Vinay <navik.vinay12@gmail.com>
2022-12-21 14:04:19 +0530

    UC5-fetching some particular or specific rows from the table

--- SQL EmployeePayRollProblem.sql
+++ SQL EmployeePayRollProblem.sql
@@ -15,4 +15,9 @@ insert into EmployeePayRoll values('Ashwin',67192.34,'2009-03-21');		--way1
 insert into EmployeePayRoll (Salary,Name,StartDate) values(46000.23,'Dhoni','2002-03-22');		--way2
 ----UC4 Reading Data from Table---
 select * from EmployeePayRoll;
+----UC 5 Read particular employee data from table----
+select * from EmployeePayRoll where name='Dhoni';
+select * from EmployeePayRoll where id=3 or id=2;
+select * from EmployeePayRoll where StartDate between '2009-02-01' and GetDate();
+select * from EmployeePayRoll where id in (1,2,3);
 
