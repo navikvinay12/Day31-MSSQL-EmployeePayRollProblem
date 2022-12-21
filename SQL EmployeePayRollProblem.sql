c0f8a9bc0902c302c0c44902063f2e3105fbde14
Vinay <navik.vinay12@gmail.com>
2022-12-21 13:41:10 +0530

    UC2 creting a new table in db

--- SQL EmployeePayRollProblem.sql
+++ SQL EmployeePayRollProblem.sql
@@ -2,4 +2,14 @@
 -----UC1 Creating Database-----
 create database PayrollService;
 use PayrollService;
+-----UC2 Creating Table in database PayRollService------
+create table EmployeePayRoll
+(
+	Id int identity(1,1) primary key,
+	Name varchar(200),
+	Salary float,
+	StartDate date
+);
+select * from EmployeePayRoll;
+
 
